6ba244cdf723792c73ef14099fc00ce29137fe2f
WojciechWeg <wojtek.weg@gmail.com>
2019-09-08 00:37:40 +0200

    validation added

--- src/main/java/com/tinybank/tinybankapi/controllers/RestResponseEnityExceptionHandler.java
+++ src/main/java/com/tinybank/tinybankapi/controllers/RestResponseEnityExceptionHandler.java
@@ -3,9 +3,11 @@ package com.tinybank.tinybankapi.controllers;
 
 import com.tinybank.tinybankapi.model.ErrorResponse;
 import com.tinybank.tinybankapi.services.ResourceNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,10 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
@@ -21,8 +26,24 @@ public class RestResponseEnityExceptionHandler extends ResponseEntityExceptionHa
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<ErrorResponse> handleCustomerNotFound(ResourceNotFoundException ex, WebRequest request){
+
+
         ErrorResponse errorResponse =
-                new ErrorResponse( LocalDateTime.now(),"Not found", "404", "Student does not exits");
+                new ErrorResponse( HttpStatus.NOT_FOUND, ex.getMessage(), Arrays.asList("Customer does not exits"));
         return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<String> errorList = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errorList);
+        return handleExceptionInternal(ex, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
     }
 }
